06a8e8b0daac7c7ca8db0bbf18ab84688e8d91c3
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-12-26 08:23:00 +0600

    Initial commit

---
+++ eldragon.py
@@ -0,0 +1,32 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import pathlib
+import random
+
+# Setting the chrome_options
+global chrome_options
+chrome_options = Options()
+scriptDirectory = pathlib.Path().absolute()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--user-data-dir=chrome-data")
+chrome_options.add_argument('--profile-directory=Profile 8')
+prefs = {"profile.default_content_setting_values.notifications": 2}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument('disable-infobars')
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("user-data-dir=chrome-data")
+chrome_options.add_argument(f"user-data-dir={scriptDirectory}\\userdata")
+
+driver = webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
+
+
+# print(input(" Connect your waller address :"))
+driver.implicitly_wait(10)
+driver.get("https://marketplace.thetanarena.com/")
+
+print(input("Start Project ..... :"))
